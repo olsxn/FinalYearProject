193db13438309c93125511278febe2684dafd439
Halil Olsen <olsenhalil@gmail.com>
2020-03-31 14:28:45 +0100

    2.3

--- index.php
+++ index.php
@@ -54,7 +54,57 @@ CloseCon($conn);
     </div>
 
 
+	<!-- Trigger/Open The Modal -->
+	<button id="myBtn">Open Modal</button>
+	<!-- The Modal -->
+	<div id="myModal" class="modal">
+		<!-- Modal content -->
+		<div class="modal-content">
+			<div class="modal-header">
+			<h2>Modal Header</h2>
+			<span class="close">&times;</span>
+			</div>
+			<div class="modal-body">
+			<p>Some text in the Modal Body</p>
+			<p>Some other text...</p>
+			</div>
+			<div class="modal-footer">
+			<button>Click here to go to next modal</button>
+			</div>
+		</div>
+	</div>
+
+	
+
 <!-- scripts here -->
+<!-- modal JS -->
+	<script>
+		// Get the modal
+		var modal = document.getElementById("myModal");
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("myBtn");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on the button, open the modal
+		btn.onclick = function() {
+		modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+		}
+	</script>
 	<!-- close and open box -->
 	<script type="text/javascript">
 			var c = 0;
@@ -68,6 +118,7 @@ CloseCon($conn);
 					c = 0;
 				}
 			}
+			
 	</script>
 	<!-- bootstrap 4 scripts -->
 	<script src="jquery-3.4.1.min.js"></script>
